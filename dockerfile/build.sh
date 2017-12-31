9e8bd21508dd0827e6b20e4f31f2f8841e201286
yehiyam <yehiyam@gmail.com>
2017-12-31 15:30:56 +0200

    add CI

---
+++ dockerfile/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+REPO_NAME=$1
+if [ -v PRIVATE_REGISTRY ]
+then
+  IMAGE_NAME=${PRIVATE_REGISTRY}/${REPO_NAME}
+else
+  IMAGE_NAME=sound/${REPO_NAME}
+fi
+
+VERSION="v${npm_package_version}"
+TAG_VER="${IMAGE_NAME}:${VERSION}"
+TAG_CUR="${IMAGE_NAME}:latest"
+
+docker build -t ${TAG_VER} -t ${TAG_CUR} -f ./dockerfile/Dockerfile .
+if [ -v PRIVATE_REGISTRY ]
+then
+  docker push ${TAG_VER}
+  docker push ${TAG_CUR}
+fi
+
