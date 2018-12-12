90a88c731db6a7b2a5ca8842b4cb7da1a0752370
maty21 <maty21@gmail.com>
2018-12-12 15:27:43 +0200

    update version add scripts add static server

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const DEFAULT_PORT = parseInt(process.env.PORT) || 9050;
+const parseBool = (value) => {
+    if (typeof value === 'boolean') {
+        return value;
+    }
+    if (typeof value === 'string' && value.toLowerCase() === 'false') {
+        return false;
+    }
+    return true;
+}
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+app.get('/config', (req, res) => {
+    res.json({
+        config: {
+            monitorBackend: {
+                useLocation: parseBool(process.env.MONITOR_BACKEND_USE_LOCATION),
+                host: process.env.MONITOR_BACKEND_HOST || 'localhost',
+                port: process.env.MONITOR_BACKEND_PORT || '30010',
+                path: process.env.MONITOR_BACKEND_PATH || '',
+                socketIoPath: process.env.MONITOR_BACKEND_PATH_SOCKETIO || '',
+                schema: process.env.isSecure ? 'https://' : 'http://'
+            }
+        }
+    })
+})
+const server = http.createServer(app);
+server.listen(DEFAULT_PORT, () => {
+    console.log('Listening on port', DEFAULT_PORT);
+});
