33a1dbb1dbd94d4e60e56007a4561b5da15dd70a
Yehiyam Livneh <yehiyam@gmail.com>
2019-08-28 10:37:05 +0300

    fix CI

--- scripts/updateDeploymentStatus.sh
+++ scripts/updateDeploymentStatus.sh
@@ -1,4 +1,4 @@
-curl -X POST https://api.github.com/repos/kube-HPC/${TRAVIS_REPO_SLUG}/deployments/${DEPLOYMENT_ID}/statuses \
+curl -X POST https://api.github.com/repos/${TRAVIS_REPO_SLUG}/deployments/${DEPLOYMENT_ID}/statuses \
      -H "Authorization: token ${GH_TOKEN}" \
      -H "Content-Type: text/json; charset=utf-8" \
      -H "Accept: application/vnd.github.flash-preview+json" \
