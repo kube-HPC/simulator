da29b8d2a6048e47e3a8767c093276cdd1309469
denvash <dennisvash@gmail.com>
2019-11-20 18:55:22 +0200

    [+] JsonTable

--- src/components/common/Json/JsonTable/JsonTable.react.js
+++ src/components/common/Json/JsonTable/JsonTable.react.js
@@ -2,18 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'components/common/Card.react';
 
+const isPureObject = obj => !Array.isArray(obj) && typeof obj === 'object' && obj !== null;
+
+const recursionStep = value =>
+  isPureObject(value) ? (
+    objToTable(value)
+  ) : Array.isArray(value) ? (
+    value.map(recursionStep)
+  ) : (
+    <td>{value}</td>
+  );
+
+function objToTable(obj) {
+  return (
+    <tbody>
+      {Object.entries(obj).map(([key, value]) => (
+        <tr key={key}>
+          <th>{key}</th>
+          <tr>{recursionStep(value)}</tr>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
 const JsonTable = ({ jsonObject }) => (
-  <Card bordered={false}>
-    <table>
-      <tbody>
-        {Object.entries(jsonObject).map(([key, value], i) => (
-          <tr key={i}>
-            <td>{key}</td>
-            <td>{value}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  <Card>
+    <table>{objToTable(jsonObject)}</table>
   </Card>
 );
 
