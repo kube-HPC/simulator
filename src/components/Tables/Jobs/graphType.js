72fa5bae8b26358283b50ca9a5cc3daeaec16fc7
Nassi Harel <nassi9@gmail.com>
2019-12-11 12:55:20 +0200

    fix: improve graph

---
+++ src/components/Tables/Jobs/graphType.js
@@ -0,0 +1,28 @@
+export default {
+  STATUS: {
+    NOT_STARTED: 'notStarted',
+    RUNNING: 'running',
+    COMPLETED: 'completed',
+    SUCCEED: 'succeed',
+    FAILED: 'failed',
+    CREATING: 'creating',
+    PENDING: 'pending',
+    SKIPPED: 'skipped',
+  },
+  BATCH: {
+    NOT_STARTED: 'batchNotStarted',
+    RUNNING: 'batchRunning',
+    COMPLETED: 'batchCompleted',
+    ERRORS: 'batchErrors',
+  },
+  SINGLE: {
+    NOT_STARTED: 'notStarted',
+    RUNNING: 'running',
+    COMPLETED: 'completed',
+  },
+  EDGE: {
+    WAIT_ANY: 'waitAny',
+    ALGORITHM_EXECUTION: 'algorithmExecution',
+    NONE: 'none',
+  },
+};
