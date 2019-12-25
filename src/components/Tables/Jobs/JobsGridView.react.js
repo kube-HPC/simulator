bd2b612683c0ef89133d973e174e28c0244c1a55
denvash <dennisvash@gmail.com>
2019-12-25 12:51:30 +0200

    [+] Show all jobs

--- src/components/Tables/Jobs/JobsGridView.react.js
+++ src/components/Tables/Jobs/JobsGridView.react.js
@@ -84,7 +84,7 @@ const toGrid = dataSource =>
 
 const JobsGridView = () => {
   const { dataSource } = useJobs();
-  return <>{toGrid(dataSource.slice(0, 10))}</>;
+  return <>{toGrid(dataSource)}</>;
 };
 
 export default memo(JobsGridView);
