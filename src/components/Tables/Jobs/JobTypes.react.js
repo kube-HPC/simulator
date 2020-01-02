c127edc97022e0617e8a453f876bbb367f3a3221
denvash <dennisvash@gmail.com>
2020-01-02 17:39:20 +0200

    [~] No tooltip on full name

--- src/components/Tables/Jobs/JobTypes.react.js
+++ src/components/Tables/Jobs/JobTypes.react.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Tag, Tooltip } from 'antd';
-import { COLOR_PIPELINE_TYPES } from 'styles';
 import { FlexBox } from 'components/common';
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+import { COLOR_PIPELINE_TYPES } from 'styles';
 import { toUpperCaseFirstLetter as toUpper } from 'utils';
 
+const Overflow = styled(FlexBox.Auto)`
+  overflow: auto;
+  flex-wrap: wrap;
+`;
+
 const JobTypes = ({ types, fullName = true }) => (
-  <FlexBox.Auto justify="center" gutter={0}>
-    {types.map(type => (
-      <Tooltip key={type} placement="top" title={toUpper(type)}>
-        <Tag color={COLOR_PIPELINE_TYPES[type]}>{toUpper(fullName ? type : type.slice(0, 2))}</Tag>
-      </Tooltip>
-    ))}
-  </FlexBox.Auto>
+  <Overflow justify="center" gutter={0}>
+    {types.map(type =>
+      fullName ? (
+        <Tag key={type} color={COLOR_PIPELINE_TYPES[type]}>
+          {toUpper(type)}
+        </Tag>
+      ) : (
+        <Tooltip key={type} placement="top" title={toUpper(type)}>
+          <Tag color={COLOR_PIPELINE_TYPES[type]}>{toUpper(type.slice(0, 2))}</Tag>
+        </Tooltip>
+      ),
+    )}
+  </Overflow>
 );
 
 JobTypes.propTypes = {
