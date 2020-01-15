5dd4a676a84fcdd2b23e177aaaade5479af92b27
denvash <dennisvash@gmail.com>
2020-01-15 15:06:04 +0200

    [-] slice

--- src/reducers/jobs.reducer.js
+++ src/reducers/jobs.reducer.js
@@ -8,7 +8,7 @@ export const jobsTable = handleActions(
   {
     [actions.SOCKET_GET_DATA](currJobs, { payload }) {
       const { jobs } = payload;
-      return Immutable.from({ dataSource: jobs.slice(0, 5) });
+      return Immutable.from({ dataSource: jobs });
     },
   },
   Immutable.from({ dataSource: undefined }),
