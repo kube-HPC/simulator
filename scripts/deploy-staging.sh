3d0bf2d757fff9c689b2275f34f44ab4a2471e9a
yehiyam <yehiyam@users.noreply.github.com>
2020-01-28 09:38:26 +0200

    preload monaco editor (#86)
    * preload monaco editor
    
    * script fix
    
    * ci fix

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -2,7 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "export PATH=$PWD/bin:$PATH" > setPath
 source ./setPath
-export PR_NAME="${TRAVIS_PULL_REQUEST_BRANCH,,}"; export PR_NAME="${PR_NAME//_/-}"
+echo TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+export PR_NAME="${TRAVIS_PULL_REQUEST_BRANCH,,}"
+export PR_NAME="${PR_NAME//_/-}"
+echo TAG=$PR_NAME
 envsubst < ${DIR}/staging-template.yaml > ~/staging.yaml
 kubectl apply -f ~/staging.yaml
 kubectl patch deployment simulator-${PR_NAME} -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$(date +%s)\"}}}}}"
