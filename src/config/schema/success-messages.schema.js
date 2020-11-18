c7484bcebd3246cad7c66f1c387738ad261a31ba
reggev <reggev.am@gmail.com>
2020-11-18 11:13:38 +0200

    added notification on success

--- src/config/schema/success-messages.schema.js
+++ src/config/schema/success-messages.schema.js
@@ -5,14 +5,15 @@ const successMsg = payload => ({
   ALGORITHM_APPLY: 'Algorithm Applied, check Algorithms table',
   ALGORITHM_RUN: (
     <>
-      Algorithm <Text code>{payload.body && payload.body.name}</Text> started, check{' '}
-      <Text strong>Jobs</Text> table.
+      Algorithm <Text code>{payload.body && payload.body.name}</Text> started,
+      check <Text strong>Jobs</Text> table.
     </>
   ),
   BUILD_RERUN: 'Build rerun started',
   BUILD_STOP: 'Build has stopped',
   CRON_START: 'Cron job started for selected pipeline',
   CRON_STOP: 'Cron job disabled for selected pipeline',
+  DATASOURCE_CREATE: `created dataSource ${payload.name}`,
   DEBUG_ADD: 'Debug added',
   DEBUG_DELETE: 'Debug deleted',
   JOBS_EXEC_CACHING: `Jobs Caching Execution Succeed, executing job with ID=${payload.jobId}`,
