d61d9f4a2868c6e859e484880713097dcb9c32d6
reggev <reggev.am@gmail.com>
2020-11-23 19:26:34 +0200

    cleanups

--- src/Routes/Tables/Pipelines/useActivePipeline.js
+++ src/Routes/Tables/Pipelines/useActivePipeline.js
@@ -1,22 +1,12 @@
-// import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-// import { tableFilterSelector } from 'utils/tableSelector';
-// import { LEFT_SIDEBAR_NAMES } from 'const';
 import { selectors } from 'reducers/pipeline.reducer';
 import usePath from './usePath';
-
-// const dataSelector = tableFilterSelector(LEFT_SIDEBAR_NAMES.PIPELINES);
 
 export default () => {
   const { pipelineId } = usePath();
   const pipeline = useSelector(state =>
     selectors.collection.byId(state, pipelineId)
   );
-
-  // const pipeline = useMemo(
-  //   () => dataSource.find(item => item.name === pipelineId),
-  //   [dataSource, pipelineId]
-  // );
 
   return { pipeline, pipelineId };
 };
