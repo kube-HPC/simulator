3a53b316a8268f97558adf17dc41eafac2cd0c40
reggev <reggev.am@gmail.com>
2020-11-24 15:37:17 +0200

    added fast escape on unset filters

--- src/hooks/useJobs.js
+++ src/hooks/useJobs.js
@@ -11,19 +11,18 @@ const useJobs = () => {
   const searchFilter = useSelector(state => state.autoCompleteFilter.filter);
   const { filters } = useFilters();
 
-  const filtered = useMemo(
-    () =>
-      collection.filter(item => {
-        // instead of useSearch to avoid looping the array twice
-        if (searchFilter !== '' && !item.key.includes(searchFilter))
-          return false;
-        if (filters.length === 0) return true;
-        // avoid running through the tags if there aren't enough anyway
-        if (item.pipeline.types.length < filters.length) return false;
-        return filters.every(tag => item.pipeline.types.includes(tag));
-      }),
-    [collection, searchFilter, filters]
-  );
+  const filtered = useMemo(() => {
+    if (searchFilter === '' && filters.length === 0) return collection;
+
+    return collection.filter(item => {
+      // instead of useSearch to avoid looping the array twice
+      if (searchFilter !== '' && !item.key.includes(searchFilter)) return false;
+      if (filters.length === 0) return true;
+      // avoid running through the tags if there aren't enough anyway
+      if (item.pipeline.types.length < filters.length) return false;
+      return filters.every(tag => item.pipeline.types.includes(tag));
+    });
+  }, [collection, searchFilter, filters]);
 
   const { loading } = useSelector(
     state => state[STATE_SOURCES.EXPERIMENTS],
