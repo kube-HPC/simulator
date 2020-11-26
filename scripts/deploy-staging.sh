34303b21b694e8c57e1eb53e843ae35f9035a5af
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-26 17:23:00 +0200

    deploy

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -3,7 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "export PATH=$PWD/bin:$PATH" > setPath
 source ./setPath
 echo TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
-export PR_NAME="${TRAVIS_PULL_REQUEST_BRANCH,,}"
+# export PR_NAME="${TRAVIS_PULL_REQUEST_BRANCH,,}"
+export PR_NAME="${PR_NAME,,}"
 export PR_NAME="${PR_NAME//_/-}"
 echo TAG=$PR_NAME
 envsubst < ${DIR}/staging-template.yaml > ~/staging.yaml
