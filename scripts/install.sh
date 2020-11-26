d87acf3c6e00efeec7c505beb0cbbbd35b58ad4c
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-26 18:11:25 +0200

    ttt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
-
+set +x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.4/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p ./bin
 echo "export PATH=$PWD/bin:$PATH" > setPath
