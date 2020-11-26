8e7b38e8884e4229d9bc52fa79c3b08cbd21179c
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-26 19:01:42 +0200

    xxx

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo "export PATH=$PWD/bin:$PATH" > setPath
-source ./setPath
+# echo "export PATH=$PWD/bin:$PATH" > setPath
+# source ./setPath
 echo TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
 # export PR_NAME="${TRAVIS_PULL_REQUEST_BRANCH,,}"
 export PR_NAME="${PR_NAME,,}"
 export PR_NAME="${PR_NAME//_/-}"
 echo TAG=$PR_NAME
-envsubst < ${DIR}/staging-template.yaml > ~/staging.yaml
-kubectl apply -f ~/staging.yaml
+envsubst < ${DIR}/staging-template.yaml > /tmp/staging.yaml
+kubectl apply -f /tmp/staging.yaml
 kubectl patch deployment simulator-${PR_NAME} -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$(date +%s)\"}}}}}"
