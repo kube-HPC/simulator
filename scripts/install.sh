06156a3cebe2092fa6f25ad9e4ca6434c337135a
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-26 18:53:46 +0200

    sss

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,12 @@
 
 #!/bin/bash
-set +x
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $DIR
-mkdir -p ./bin
-echo 222
-curl -L --output ./bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.19.4/bin/linux/amd64/kubectl 
-echo 333
-chmod +x ./bin/kubectl
-echo 444
-echo "export PATH=$PWD/bin:$PATH" > setPath
-echo 555
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ~/kubectl
+mkdir -p ~/bin
+echo "export PATH=$HOME/bin:$PATH" > setPath
 source ./setPath
-echo 666
-mkdir -p ~/.kube/
-echo 777
-envsubst < ${DIR}/kube-config-template.yml > ~/.kube/config
-echo 888
+mv ./kubectl $HOME/bin/kubectl
 kubectl cluster-info
-echo 999
