d9d0a3c171d52187d9ff136a3fc96ec609f1e4ef
reggev <reggev.am@gmail.com>
2020-12-08 14:34:28 +0200

    filled in missing types

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -8,7 +8,12 @@ const TYPES = {
 };
 
 const msg = 'Something Went Wrong!';
-
+/**
+ * @param {object} props
+ * @param {'success' | 'info' | 'warning' | 'error'} props.type
+ * @param {string} props.message
+ * @param {string} props.description
+ */
 const notification = ({ message = msg, description, type = TYPES.ERROR }) =>
   notificationAntd[type]({
     message,
