f08e26d1b4b7c7f8caa415da419ed8d76da759b0
reggev <reggev.am@gmail.com>
2021-01-24 12:11:12 +0200

    upodated selectors

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,12 +1,13 @@
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { selectors } from 'reducers';
 
 /**
  * @template T
  * @type {(collection: T[], key: string) => T[]}
  */
 export const useFilter = (collection, key) => {
-  const filter = useSelector(state => state.autoCompleteFilter.filter);
+  const filter = useSelector(selectors.autoCompleteFilter);
   const data = useMemo(
     () => collection.filter(item => item[key].includes(filter)),
     [collection, key, filter]
