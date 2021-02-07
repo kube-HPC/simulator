b723c5b0df14a8242b0d4c45b14f019699cae767
Yehiyam Livneh <yehiyam@gmail.com>
2021-02-07 15:09:14 +0200

    prop types

---
+++ src/components/DownloadLink.js
@@ -0,0 +1,52 @@
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { selectors } from 'reducers';
+
+/**
+ * Renders a download link and clicks the link automatically when a non null
+ * href value is set. accepts a ref with a "download" callback to manually download
+ */
+// eslint-disable-next-line
+const DownloadLink = ({ href, autoDownload }, ref) => {
+  const { socketUrl } = useSelector(selectors.connection);
+  const linkRef = useRef();
+
+  const download = useCallback(
+    () => href !== null && linkRef.current && linkRef.current.click(),
+    [href, linkRef]
+  );
+
+  useEffect(() => {
+    if (!autoDownload) return;
+    download();
+  }, [download, autoDownload]);
+
+  useImperativeHandle(ref, () => ({ download }));
+  return href ? (
+    <a
+      style={{ display: 'none' }}
+      ref={linkRef}
+      href={`${socketUrl}/${href}`}
+      download="file.zip">
+      hidden download link
+    </a>
+  ) : null;
+};
+
+const WrappedDownloadLink = forwardRef(DownloadLink);
+
+WrappedDownloadLink.propTypes = {
+  href: PropTypes.string,
+  autoDownload: PropTypes.bool,
+};
+
+WrappedDownloadLink.defaultProps = { href: null, autoDownload: true };
+
+export default WrappedDownloadLink;
