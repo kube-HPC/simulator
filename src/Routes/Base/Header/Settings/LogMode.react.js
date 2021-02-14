570854b25fbe8b0d533b0fff1d2db317e2b64b53
Yehiyam Livneh <yehiyam@gmail.com>
2021-02-14 13:11:46 +0200

    CR fixes

--- src/Routes/Base/Header/Settings/LogMode.react.js
+++ src/Routes/Base/Header/Settings/LogMode.react.js
@@ -1,26 +1,30 @@
 import { Radio, Typography } from 'antd';
+import { logModes } from '@hkube/consts';
 import { FlexBox } from 'components/common';
 import { useSettings } from 'hooks';
-import { logModeTypes } from 'config/template/log-mode.template';
 import React, { memo, useCallback } from 'react';
 
 const LogMode = () => {
   const { logMode, setSettings } = useSettings();
 
   const onChange = useCallback(
-    // eslint-disable-next-line
-    ({ target: { value: logMode } }) => setSettings({ logMode }),
+    ({ target: { value: logModeValue } }) =>
+      setSettings({ logMode: logModeValue }),
     [setSettings]
   );
   return (
     <FlexBox.Auto>
       <Typography.Text strong>Log Mode</Typography.Text>
       <Radio.Group value={logMode} onChange={onChange}>
-        {Object.entries(logModeTypes).map(([key, value]) => (
-          <Radio key={key} value={value}>
-            {key}
-          </Radio>
-        ))}
+        <Radio key="log-mode-Algorithm" value={logModes.ALGORITHM}>
+          Algorithm
+        </Radio>
+        <Radio key="log-mode-Internal" value={logModes.INTERNAL}>
+          Internal
+        </Radio>
+        <Radio key="log-mode-All" value={logModes.ALL}>
+          All
+        </Radio>
       </Radio.Group>
     </FlexBox.Auto>
   );
--- src/config/template/log-mode.template.js
+++
@@ -1,7 +0,0 @@
-import { logModes } from '@hkube/consts';
-
-export const logModeTypes = {
-  Algorithm: logModes.ALGORITHM,
-  Internal: logModes.INTERNAL,
-  All: logModes.ALL,
-};
