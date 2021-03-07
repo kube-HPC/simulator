2b43d63ded393ce72b4f39f9ef101aea2fb480a2
Yehiyam Livneh <yehiyam@gmail.com>
2021-03-07 17:16:58 +0200

    fix crash in graph view

--- src/components/common/Json/JsonTable/JsonTable.react.js
+++ src/components/common/Json/JsonTable/JsonTable.react.js
@@ -23,96 +23,89 @@ const isEmptyObject = obj => Object.entries(obj).length === 0;
 const Margin = styled(Descriptions)`
   margin-top: ${prop('isMargin', 'none')};
 `;
-
-// Recursion Step
-const RenderItemByValueType = ({
-  obj,
-  vertical,
-  isMargin = false,
-  key,
-  jobId,
-}) => {
+// Entry
+const JsonTable = ({ obj: mainObj, vertical = false, jobId, ...props }) => {
   const socketUrl = useSelector(state => state[STATE_SOURCES.SOCKET_URL]);
   const downloadLinkRef = useRef();
   const handleDownload = useCallback(
     () => downloadLinkRef.current && downloadLinkRef.current?.click(),
     [downloadLinkRef]
   );
+  // Recursion Step
+  const RenderItemByValueType = ({ obj, isMargin = false, key }) => {
+    if (isPureObject(obj)) {
+      if (key === 'flowInput' && obj.truncated) {
+        return (
+          <>
+            <Button onClick={handleDownload}>Download</Button>
+            <a
+              style={{ display: 'none' }}
+              ref={downloadLinkRef}
+              href={`${socketUrl}/flowInput/${jobId}?download=true`}
+              download>
+              hidden download link
+            </a>
+          </>
+        );
+      }
 
-  if (isPureObject(obj)) {
-    if (key === 'flowInput' && obj.truncated) {
+      return (
+        <Margin
+          key={key}
+          column={getTotalColumns({ obj, vertical })}
+          vertical={vertical}
+          isMargin={isMargin}>
+          {objToItem({ obj })}
+        </Margin>
+      );
+    }
+    if (Array.isArray(obj)) {
       return (
         <>
-          <Button onClick={handleDownload}>Download</Button>
-          <a
-            style={{ display: 'none' }}
-            ref={downloadLinkRef}
-            href={`${socketUrl}/flowInput/${jobId}?download=true`}
-            download>
-            hidden download link
-          </a>
+          {obj.map((value, i) =>
+            RenderItemByValueType({
+              obj: value,
+              vertical,
+              isMargin: i !== 0 || i === obj.length - 1,
+              key: i,
+              jobId,
+            })
+          )}
         </>
       );
     }
+    return String(obj);
+  };
 
-    return (
-      <Margin
-        key={key}
-        column={getTotalColumns({ obj, vertical })}
-        vertical={vertical}
-        isMargin={isMargin}>
-        {objToItem({ obj })}
-      </Margin>
-    );
-  }
-  if (Array.isArray(obj)) {
-    return (
-      <>
-        {obj.map((value, i) =>
-          RenderItemByValueType({
-            obj: value,
-            vertical,
-            isMargin: i !== 0 || i === obj.length - 1,
-            key: i,
-            jobId,
-          })
+  RenderItemByValueType.propTypes = {
+    obj: PropTypes.object,
+    vertical: PropTypes.bool,
+    isMargin: PropTypes.bool,
+    key: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+    jobId: PropTypes.string,
+  };
+
+  // Item
+  function objToItem({ obj }) {
+    return Object.entries(obj).map(([key, value]) => (
+      <Descriptions.Item key={key} label={<Text strong>{key}</Text>}>
+        {isPureObject(value) && isEmptyObject(value) ? (
+          <Tag>{EMPTY}</Tag>
+        ) : (
+          <RenderItemByValueType obj={value} key={key} jobId={jobId} />
         )}
-      </>
-    );
+      </Descriptions.Item>
+    ));
   }
-  return String(obj);
+
+  return (
+    <Descriptions
+      column={getTotalColumns({ obj: mainObj, vertical })}
+      vertical={vertical}>
+      {objToItem({ obj: mainObj, vertical, jobId })}
+    </Descriptions>
+  );
 };
-
-RenderItemByValueType.propTypes = {
-  obj: PropTypes.object,
-  vertical: PropTypes.bool,
-  isMargin: PropTypes.bool,
-  key: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
-  jobId: PropTypes.string,
-};
-
-// Item
-function objToItem({ obj, vertical, jobId }) {
-  return Object.entries(obj).map(([key, value]) => (
-    <Descriptions.Item key={key} label={<Text strong>{key}</Text>}>
-      {isPureObject(value) && isEmptyObject(value) ? (
-        <Tag>{EMPTY}</Tag>
-      ) : (
-        RenderItemByValueType({ obj: value, vertical, key, jobId })
-      )}
-    </Descriptions.Item>
-  ));
-}
-
-// Entry
-const JsonTable = ({ obj, vertical = false, jobId, ...props }) => (
-  <Descriptions
-    column={getTotalColumns({ obj, vertical })}
-    vertical={vertical}
-    {...props}>
-    {objToItem({ obj, vertical, jobId })}
-  </Descriptions>
-);
 
 JsonTable.propTypes = {
   obj: PropTypes.object,
