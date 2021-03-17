fbfddaf82620b9db3b683e5ff9505aa3e760eae7
reggev <reggev.am@gmail.com>
2021-03-17 17:10:05 +0200

    added RemoteDetails panel

--- src/Routes/Tables/DataSources/EditDrawer/Header.js
+++ src/Routes/Tables/DataSources/EditDrawer/Header.js
@@ -1,62 +1,29 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Button, Tooltip } from 'antd';
-import { copyToClipboard } from 'utils';
+import RemoteDetails from './RemoteDetails';
 
 const options = {
   SUCCESS: (_, dataSourceName) => dataSourceName,
   PENDING: () => 'loading...',
 };
 
-const ButtonContainer = styled.div`
-  margin-left: auto;
-`;
-
 const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
 `;
 
-const Header = ({ status, dataSourceId, dataSourceName, git, storage }) => {
-  const onCopyRepositoryUrl = useCallback(() => {
-    copyToClipboard(git.repositoryUrl);
-  }, [git]);
-
-  const onCopyBucket = useCallback(() => {
-    if (storage.kind === 'S3')
-      copyToClipboard(
-        `endpoint: ${storage.endpoint}, bucketName: ${storage.bucketName}`
-      );
-  }, [storage]);
-
-  return ['FAIL', 'NOT_FOUND'].includes(status) ? null : (
+const Header = ({ status, dataSourceId, dataSourceName, git, storage }) =>
+  ['FAIL', 'NOT_FOUND'].includes(status) ? null : (
     <HeaderContainer>
       <h2>
         {options[status] ? options[status](dataSourceId, dataSourceName) : ''}
       </h2>
-      <ButtonContainer>
-        {storage.kind === 'S3' ? (
-          <Tooltip title="copy storage info" placement="left">
-            <Button onClick={onCopyBucket} type="dashed">
-              S3
-            </Button>
-          </Tooltip>
-        ) : null}
-        {git.kind !== 'internal' ? (
-          <Tooltip title="copy repository url" placement="left">
-            <Button
-              style={{ marginLeft: '0.5ch' }}
-              onClick={onCopyRepositoryUrl}
-              icon="branches"
-              type="dashed"
-            />
-          </Tooltip>
-        ) : null}
-      </ButtonContainer>
+      {storage.kind !== 'internal' || git.kind !== 'internal' ? (
+        <RemoteDetails storage={storage} git={git} />
+      ) : null}
     </HeaderContainer>
   );
-};
 
 Header.propTypes = {
   status: PropTypes.string.isRequired,
