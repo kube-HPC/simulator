919e1658c37ead2913d239a4a365df46b3caf25d
reggev <reggev.am@gmail.com>
2021-03-25 16:03:13 +0200

    fixed drawerEditor height

--- src/components/Drawer/DrawerEditor.react.js
+++ src/components/Drawer/DrawerEditor.react.js
@@ -30,7 +30,7 @@ const DrawerEditor = ({ value: initial, submitText, onSubmit }) => {
 
   return (
     <>
-      <Card style={{ flex: 1 }}>
+      <Card style={{ flex: 1 }} bodyStyle={{ height: '100%' }}>
         <JsonEditor innerRef={editorRef} value={value} />
       </Card>
       <BottomPanel style={{ marginTop: '1em' }}>
