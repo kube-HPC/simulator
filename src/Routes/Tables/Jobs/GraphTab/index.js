54f38b89bc1b74299e38b0088078d5a52a495743
Ziv Glik <zivglik@gmail.com>
2022-08-04 09:31:23 +0300

    fix button debug or run job in popup graph job

--- src/Routes/Tables/Jobs/GraphTab/index.js
+++ src/Routes/Tables/Jobs/GraphTab/index.js
@@ -89,17 +89,17 @@ const GraphTab = ({ graph, pipeline }) => {
   );
 
   const isDisabledBtnRunDebug = useMemo(() => {
-    let res = false;
-    const nodesNames = pipeline.nodes.map(item => item.nodeName);
+    const res = false;
+    // const nodesNames = pipeline.nodes.map(item => item.nodeName);
 
-    if (
+    /*  if (
       pipeline?.kind === 'stream' ||
       (node &&
         node !== {} &&
         (node?.kind !== 'algorithm' || !nodesNames.includes(node?.nodeName)))
     ) {
       res = true;
-    }
+    } */
 
     return res;
   }, [node, pipeline?.kind, pipeline.nodes]);
