af4fa070c168bcebbcfa3c4b5f6fac2d1d3216bd
golanha <golanha@rnd-hub.com>
2022-10-19 16:07:14 +0300

    adding port for monitor/api server

--- src/reducers/config.reducer.js
+++ src/reducers/config.reducer.js
@@ -21,9 +21,9 @@ const config = createSlice({
       ...state,
       backendApiUrl: payload.config.monitorBackend.useLocation
         ? payload.config.monitorBackend.path
-        : payload.config.monitorBackend.schema +
-          payload.config.monitorBackend.host +
-          payload.config.monitorBackend.path,
+        : `${payload.config.monitorBackend.schema +
+        payload.config.monitorBackend.host}:${payload.config.monitorBackend.port
+        }${payload.config.monitorBackend.path}`,
 
       baseUrl: payload.config.baseUrl,
       hasConfig: true,
