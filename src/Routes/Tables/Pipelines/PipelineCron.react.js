1df397d493c3ea95966240e526c8e29af1ca318e
ziv glik <zivglik@gmail.com>
2023-04-23 14:06:08 +0300

    add cron wizard

--- src/Routes/Tables/Pipelines/PipelineCron.react.js
+++ src/Routes/Tables/Pipelines/PipelineCron.react.js
@@ -1,18 +1,32 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { CheckOutlined } from '@ant-design/icons';
-import { Input, Popover, Switch, Typography, Tooltip } from 'antd';
+import { USER_GUIDE } from 'const';
+import { EditFilled } from '@ant-design/icons';
+import { Button, Popover, Switch, Typography, Tooltip } from 'antd';
+import Moment from 'react-moment';
 import { useActions, usePipeline } from 'hooks';
-import { FlexBox } from 'components/common';
+import { FlexBox, CronModel } from 'components/common';
 import cronParser from 'cron-parser';
 import cronstrue from 'cronstrue';
 import { notification } from 'utils';
 
 const { Text } = Typography;
+const iconSize = {
+  width: '18px',
+
+  opacity: 0.6,
+  cursor: 'pointer',
+};
+
+const CronButtonsContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+`;
 
 const DEFAULT_CRON_EXPR = '0 * * * *';
 const ERROR_CRON_EXPR = 'Invalid Cron Expression';
-const inputWidth = { width: '20ch' };
 
 const isCron = pipeline =>
   pipeline.triggers && pipeline.triggers.cron && pipeline.triggers.cron.enabled;
@@ -22,19 +36,11 @@ const isPattern = pipeline =>
   pipeline.triggers.cron &&
   !!pipeline.triggers.cron.pattern;
 
-// the extra div is required for the tooltip to render
-const enterButton = (
-  <Tooltip title="save changes">
-    <div>
-      <CheckOutlined />
-    </div>
-  </Tooltip>
-);
-
 const PipelineCron = ({ pipeline }) => {
+  const [isModalCronOpen, setIsModalCronOpen] = useState(false);
+  const [cronIsEnabled, setCronIsEnabled] = useState(isCron(pipeline));
   const { cronStart, cronStop } = useActions();
   const { updateCron } = usePipeline();
-  const cronIsEnabled = isCron(pipeline);
   const prevCronRef = useRef(cronIsEnabled);
   const patternIsPresent = isPattern(pipeline);
 
@@ -45,12 +51,14 @@ const PipelineCron = ({ pipeline }) => {
     : DEFAULT_CRON_EXPR;
 
   const [value, setValue] = useState(cronExpr);
+  // const [intervalCronText, setIntervalCronText] = useState(cronParser.parseExpression(value));
 
   useEffect(() => {
     setToggleLoading(false);
   }, [cronIsEnabled, prevCronRef, setToggleLoading]);
 
   let renderTooltip = ERROR_CRON_EXPR;
+  let renderTime = ERROR_CRON_EXPR;
 
   try {
     const interval = cronParser.parseExpression(value);
@@ -63,16 +71,34 @@ const PipelineCron = ({ pipeline }) => {
         , Next Interval:<Text code>{interval.next().toString()}</Text>
       </Text>
     );
+
+    renderTime = (
+      <Text>
+        Next:{' '}
+        <Moment format="DD/MM/YY HH:mm:ss">{interval.next().toString()}</Moment>
+      </Text>
+    );
   } catch (errorMessage) {
     // No need,
     // On error `renderToolTip` got default value.
   }
 
   const { name } = pipeline;
+  const callbackCronStart = () => {
+    setToggleLoading(false);
+    setCronIsEnabled(true);
+  };
+
+  const callbackCronStop = () => {
+    setToggleLoading(false);
+    setCronIsEnabled(false);
+  };
 
   const onToggle = useCallback(() => {
     setToggleLoading(true);
-    cronIsEnabled ? cronStop(name, value) : cronStart(name, value);
+    cronIsEnabled
+      ? cronStop(name, value, callbackCronStop)
+      : cronStart(name, value, callbackCronStart);
   }, [cronIsEnabled, cronStop, name, value, cronStart]);
 
   const onSave = useCallback(
@@ -80,6 +106,7 @@ const PipelineCron = ({ pipeline }) => {
       try {
         cronstrue.toString(pattern);
         updateCron(pipeline, pattern);
+        //  setIntervalCronText(pattern);
       } catch (errorMessage) {
         notification({ message: ERROR_CRON_EXPR, description: errorMessage });
       }
@@ -87,11 +114,20 @@ const PipelineCron = ({ pipeline }) => {
     [pipeline, updateCron]
   );
 
-  const handleChange = useCallback(
-    /** @param {import('react').SyntheticEvent} e */
-    e => setValue(e.target.value),
-    [setValue]
+  const handleCronModelChange = useCallback(
+    val => {
+      setValue(val);
+      onSave(val);
+    },
+    [onSave]
   );
+
+  const openPopupWizard = () => {
+    setIsModalCronOpen(true);
+    if (!cronIsEnabled) {
+      onToggle();
+    }
+  };
 
   return (
     <FlexBox.Auto justify="start">
@@ -104,18 +140,40 @@ const PipelineCron = ({ pipeline }) => {
         />
       </Tooltip>
 
-      <Popover content={renderTooltip} trigger="focus">
-        <Input.Search
+      <Popover content={renderTooltip} trigger="hover">
+        {cronIsEnabled ? renderTime : <Text disabled>Cron {value}</Text>}
+
+        {/* <Input.Search
           style={inputWidth}
           onChange={handleChange}
+          onClick={() => setIsModalCronOpen(true)}
+    
           value={value}
           size="small"
           disabled={!cronIsEnabled}
           placeholder="Cron Expression"
           enterButton={enterButton}
           onSearch={onSave}
-        />
+  /> */}
       </Popover>
+
+      <CronButtonsContainer>
+        <Button.Group className={USER_GUIDE.TABLE_JOB.ACTIONS_SELECT}>
+          <Tooltip title="Edit Cron">
+            <EditFilled style={iconSize} onClick={openPopupWizard} />
+          </Tooltip>
+        </Button.Group>
+      </CronButtonsContainer>
+
+      <CronModel
+        defaultValueExternal={value}
+        onChange={handleCronModelChange}
+        isShowModalExternal={isModalCronOpen}
+        setIsShowModalExternal={setIsModalCronOpen}
+        cronIsEnabled={cronIsEnabled}
+        switchLoading={loading}
+        switchOnToggle={onToggle}
+      />
     </FlexBox.Auto>
   );
 };
