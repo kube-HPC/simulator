3eb6d6c44c7c4ad98f24c693c4714ca188b643bf
zivglik <zivglik@gmail.com>
2024-09-12 16:58:16 +0300

    fix tooltip

--- src/Routes/Tables/Jobs/graphUtils/edges.js
+++ src/Routes/Tables/Jobs/graphUtils/edges.js
@@ -17,34 +17,83 @@ const scaleThroughput = CappedScale(fromScale, toScale);
 /* const _formatTitle = metrics =>
   Object.entries(metrics)
     .map(([k, v]) => `${k}:${v}`)
-    .join('\n'); */
+    .join('\n');
+*/
+const _formatToolTip = (metrics, fromNode, toNode, isStateLessView) => `
+ <pre style="padding-left:10px">
+<table>
+  <tr>
+    <td><b>${fromNode}</b> (Src)</td>
+    <td></td> 
+  </tr>
+  <tr>
+    <td>Messages produce rate:</td>
+    <td><b>${metrics.reqRate} per sec</b></td>
+  </tr>
+  <tr>
+    <td>Messages in queue:</td>
+    <td>${metrics.queueSize}</td>
+  </tr>
+    <tr>
+    <td>Avg number of messages in queue:</td>
+    <td>${metrics.avgQueueSize}</td>
+  </tr>
+  <tr>
+    <td>Time messages spent in queue:</td>
+    <td>${metrics.queueTimeMs} ms</td>
+  </tr>
 
-const _formatToolTip = (metrics, fromNode, toNode) => `
- <pre style="padding:20px">
-    <b>${fromNode}</b>
-    Messages produce rate: ${metrics.reqRate} per sec.
-    Messages in queue: ${metrics.queueSize}.
-    Time messages spent in queue: ${metrics.queueTimeMs} ms.
-    Avg number of messages in queue: ${metrics.avgQueueSize}.
-    Handle acknowledge revieved after:${metrics.roundTripTimeMs} ms.
-    Total sent: ${metrics.totalRequests}.
-    Total handle acknowledged: ${metrics.totalResponses}.
-    Dropped messages: ${metrics.totalDropped}.
-    <hr>
-    <b>${toNode}</b>
-    Message process rate: ${metrics.resRate}.
-    Number of instances: ${metrics.currentSize}.
-    Needed instances: ${metrics.requiered}.
-    Process rate: ${metrics.resRate} Messages per second.
-    Message process time: ${metrics.processingTimeMs} ms.
-  </pre>
+  <tr>
+    <td>Handle acknowledge received after:</td>
+    <td>${metrics.roundTripTimeMs} ms</td>
+  </tr>
+  <tr>
+    <td>Total sent:</td>
+    <td>${metrics.totalRequests}</td>
+  </tr>
+  <tr>
+    <td>Total handle acknowledged:</td>
+    <td>${metrics.totalResponses}</td>
+  </tr>
+  <tr>
+    <td>Dropped messages:</td>
+    <td>${metrics.totalDropped}</td>
+  </tr>
+    <tr>
+    <td colspan="2"><hr/></td>
+     
+  </tr>
+  <tr>
+    <td><b>${toNode}</b> (Dst)</td>
+    <td></td> 
+  </tr>
+  <tr>
+    <td>Message process rate:</td>
+    <td><b>${metrics.resRate} per sec</b></td>
+  </tr>
+
+  <tr style="display:${isStateLessView};">
+    <td>Number of instances:</td>
+    <td>${metrics.currentSize}</td>
+  </tr>
+  <tr style="display:${isStateLessView};">
+    <td>Needed instances:</td>
+    <td>${metrics.required}</td>
+  </tr>
+
+  <tr>
+    <td>Message process time:</td>
+    <td>${metrics.processingTimeMs} ms</td>
+  </tr>
+</table></pre>
 `;
 const htmlTitle = html => {
   const container = document.createElement('div');
   container.innerHTML = html;
   return container;
 };
-export const formatEdge = edge => {
+export const formatEdge = (edge, pipeline) => {
+  const { nodes } = pipeline;
   const { value, ...rest } = edge;
   const [group] = value?.types ?? [];
   const { metrics } = value || {};
@@ -57,7 +106,11 @@ export const formatEdge = edge => {
   let styles = {};
   if (metrics) {
     const { throughput } = metrics;
-    const title = htmlTitle(_formatToolTip(metrics, edge.from, edge.to));
+    const isStateless =
+      nodes.filter(x => x.nodeName === edge.to)[0].stateType === 'stateless';
+    const title = htmlTitle(
+      _formatToolTip(metrics, edge.from, edge.to, isStateless ? '' : 'none')
+    );
     const label = `${throughput}%`; // for debugging...
     const width = scaleThroughput(throughput) / 1.5;
     const edgeColor =
