3d2bcae273bbf4a1c28ffb46280f72c7300aed9a
zivglik <zivglik@gmail.com>
2025-01-23 10:48:02 +0200

    fix dev mode ui

--- src/Routes/Tables/Jobs/graphUtils/nodes.js
+++ src/Routes/Tables/Jobs/graphUtils/nodes.js
@@ -217,7 +217,7 @@ export const formatNode =
         meta?.extra?.batch
           ? `${meta.nodeName} (${meta.extra.batch})`
           : `${meta.nodeName}`
-      } ${node?.devMode ? '<b>(dev)</b>' : ''}`,
+      } ${node?.devMode ? '<b><i><u>(dev)</u></i></b>' : ''}`,
     };
     /** @type {NodeOptions} */
     const batchStyling = isBatchStyling
