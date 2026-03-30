ab1e0d853fcdc48f79296d219dd6b6a7236abdf2
zivglik <zivglik@gmail.com>
2026-03-30 17:53:42 +0300

    Add test IDs to UserAvatar and SettingsUser components for improved testing

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 import { Avatar, Tooltip } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { getColorByName } from 'utils';
+import IDProvider from 'IDProvider';
 
-const UserAvatar = ({ username, titleToolTip, size = 40 }) => {
+const UserAvatar = ({ testId, username, titleToolTip, size = 40 }) => {
   const isNullUser = username === null || username === undefined;
   const firstLetter = username?.charAt(0) || 'D';
   const bgColor = isNullUser
@@ -15,22 +16,25 @@ const UserAvatar = ({ username, titleToolTip, size = 40 }) => {
 
   return (
     <Tooltip title={!isNullUser && titleToolTip}>
-      <Avatar
-        style={{
-          backgroundColor: bgColor,
-          verticalAlign: 'middle',
-          textTransform: 'uppercase',
-          fontSize: size / 2,
-        }}
-        size={size}
-        icon={isNullUser ? <UserOutlined /> : null}>
-        {!isNullUser && firstLetter}
-      </Avatar>
+      <IDProvider dataTestId={testId || 'avatar'}>
+        <Avatar
+          style={{
+            backgroundColor: bgColor,
+            verticalAlign: 'middle',
+            textTransform: 'uppercase',
+            fontSize: size / 2,
+          }}
+          size={size}
+          icon={isNullUser ? <UserOutlined /> : null}>
+          {!isNullUser && firstLetter}
+        </Avatar>
+      </IDProvider>
     </Tooltip>
   );
 };
 
 UserAvatar.propTypes = {
+  testId: PropTypes.string,
   username: PropTypes.string,
   titleToolTip: PropTypes.string,
   size: PropTypes.number,
