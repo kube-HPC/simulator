8f9fb0475ca620a703776c77c77757e99289f873
zivglik <zivglik@gmail.com>
2026-05-24 11:45:15 +0300

    Refactor InactiveModeTag to use health monitoring state and improve rendering logic

--- src/Routes/Base/Header/InactiveMode.jsx
+++ src/Routes/Base/Header/InactiveMode.jsx
@@ -1,11 +1,22 @@
 import { Tag as AntTag, Tooltip, Typography } from 'antd';
+import { selectors } from 'reducers';
 import { DisconnectOutlined } from '@ant-design/icons';
 import { inactiveModeVar } from 'cache';
 import { useReactiveVar } from '@apollo/client';
+import { useSelector } from 'react-redux';
 import React from 'react';
 import styled from 'styled-components';
 
-const Tag = styled(AntTag)`
+const BaseTag = styled(AntTag)``;
+
+const SmallTag = styled(BaseTag)`
+  position: absolute;
+  right: 1em;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const BigTag = styled(BaseTag)`
   z-index: 1000;
   position: fixed;
   top: 50%;
@@ -14,6 +25,7 @@ const Tag = styled(AntTag)`
   padding: 12px 24px;
   border-radius: 16px;
   border: solid 1px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
 `;
 
 const Content = styled.span`
@@ -22,18 +34,30 @@ const Content = styled.span`
 
 const InactiveModeTag = () => {
   const inactiveMode = useReactiveVar(inactiveModeVar);
+
+  const { healthMonitoringEnabled } = useSelector(selectors.connection);
+
+  if (!inactiveMode) return null;
+
+  const Wrapper = healthMonitoringEnabled ? BigTag : SmallTag;
+
   return (
-    inactiveMode && (
-      <Tag color="green">
-        <Tooltip title="Reconnecting to Socket...">
-          <Content>
-            <Typography.Title level={2} style={{ margin: 0, padding: 0 }}>
+    <Wrapper color="green">
+      <Tooltip title="Reconnecting to Socket...">
+        <Content>
+          {healthMonitoringEnabled ? (
+            <Typography.Title level={2} style={{ margin: 0 }}>
               Inactive Mode <DisconnectOutlined style={{ marginLeft: '1ch' }} />
             </Typography.Title>
-          </Content>
-        </Tooltip>
-      </Tag>
-    )
+          ) : (
+            <>
+              <Typography.Text>Inactive Mode</Typography.Text>
+              <DisconnectOutlined style={{ marginLeft: '1ch' }} />
+            </>
+          )}
+        </Content>
+      </Tooltip>
+    </Wrapper>
   );
 };
 
