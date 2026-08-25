43ace9cf07837968c8c22dfe6c620216368f742c
itamarShaul <itsh13@gmail.com>
2026-08-25 15:26:59 +0300

    test

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -61,7 +61,7 @@ function registerValidSW(swUrl, config) {
       };
     })
     .catch(error => {
-      console.error('Error during service worker registration:', error);
+      console.error('Error during  service worker registration:', error);
     });
 }
 
